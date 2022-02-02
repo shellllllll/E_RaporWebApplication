350fbc9ad3cd07a5ba7eeecb5bf3c5168a3ad58a
ishelllllll <isheliarizuka@gmail.com>
2022-02-03 02:18:04 +0700

    edit view

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using E_RaporWebApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_RaporWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly AppDbContext _context;
+        public StudentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ValueController.cs
@@ -0,0 +1,22 @@
+﻿using E_RaporWebApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_RaporWebApplication.Controllers
+{
+    public class ValueController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ValueController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
